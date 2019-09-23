75119e8b4038f38ef8dca10ff3ad2a4d4d5bf7a0
Elfieda <angeleena.elfieda@hipbar.com>
2019-09-23 15:10:49 +0530

    more on functions

---
+++ ImplementE25.py
@@ -0,0 +1,22 @@
+import Exercise25
+
+Sentence = "All good  things come to those who wait"
+
+words = Exercise25.break_words(Sentence)
+words
+
+sorted_words = Exercise25.sort_words(words)
+sorted_words
+
+Exercise25.print_first_word(words)
+Exercise25.print_last_word(words)
+words
+
+Exercise25.print_first_word(sorted_words)
+Exercise25.print_last_word(sorted_words)
+sorted_words
+
+sorted_words = Exercise25.sort_sentence(Sentence)
+sorted_words
+Exercise25.print_first_and_last(Sentence)
+Exercise25.print_first_and_last_sorted(Sentence)
